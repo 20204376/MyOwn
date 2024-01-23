9960aaafcb9f145ad19c036721dbeab2b2e82b55
Kim Kyungyun <153585851+20204376@users.noreply.github.com>
2024-01-23 14:10:22 +0900

    Add files via upload

---
+++ Gugudan.java
@@ -0,0 +1,43 @@
+package homework1;
+
+public class Gugudan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i = 1; i < 10; i+=2) {
+			for(int j = 9; j>i; j-=2) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j < i; j++ ) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for(int i =1; i<10; i+=2) {
+			for(int j=0; j<i; j+=2) {
+				System.out.print(" ");
+			}
+			for(int j = 8; j>i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
